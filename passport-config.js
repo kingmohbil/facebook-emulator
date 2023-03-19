abdbb045d59e7331f869101c5d05d7c523fe3ed2
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-19 14:08:03 +0300

    Added the passport-config that adds the user profile image to the database

--- passport-config.js
+++ passport-config.js
@@ -8,32 +8,35 @@ const initializePassport = (passport) => {
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: '/auth/facebook/redirect',
+        profileFields: ['displayName', 'id', 'photos', 'profileUrl'],
       },
       async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
         try {
           const user = await users.findOne({ facebookId: profile.id });
           if (!user) {
             const user = await users.create({
-              id: profile.id,
-              accessToken,
-              refreshToken,
-              profile,
+              facebookId: profile.id,
+              username: profile.displayName,
+              profile: {
+                picture: profile._json.picture.data.url,
+              },
             });
             return done(null, user);
           }
           return done(null, user);
         } catch (error) {
-          return done(err);
+          return done(error);
         }
       }
     )
   );
 
-  passport.serializeUser((user, done) => done(null, user.id));
+  passport.serializeUser((user, done) => done(null, user._id));
 
   passport.deserializeUser(async (id, done) => {
     try {
-      const user = users.findOne({ _id: id });
+      const user = await users.findOne({ _id: id });
       return done(null, user);
     } catch (error) {
       return done(err);
