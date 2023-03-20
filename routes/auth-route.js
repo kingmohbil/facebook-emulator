6d3e80c65bf18375cbff9e323a13fdac7553b865
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-20 20:06:57 +0300

    Added the protection for the routes

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+router.use(protect);
 router.get(
   '/facebook',
   passport.authenticate('facebook', {
@@ -18,3 +19,8 @@ router.get(
 );
 
 module.exports = router;
+
+function protect(req, res, next) {
+  if (!req.user) return next();
+  return res.redirect('/');
+}
